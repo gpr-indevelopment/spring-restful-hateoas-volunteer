66bed2b5b21009a6dba16585be3b448620bd244a
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2020-10-17 11:04:40 -0300

    Spring initializer

---
+++ src/main/java/com/springrestfulhateoasvolunteer/SpringRestfulHateoasVolunteerApplication.java
@@ -0,0 +1,13 @@
+package com.springrestfulhateoasvolunteer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestfulHateoasVolunteerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestfulHateoasVolunteerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springrestfulhateoasvolunteer/SpringRestfulHateoasVolunteerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springrestfulhateoasvolunteer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestfulHateoasVolunteerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
