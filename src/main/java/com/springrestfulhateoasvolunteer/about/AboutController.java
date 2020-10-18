2ebb8de066971efd3fb1424db634ae47f0a7d075
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2020-10-18 14:11:01 -0300

    Simplified locale impl

--- src/main/java/com/springrestfulhateoasvolunteer/about/AboutController.java
+++ src/main/java/com/springrestfulhateoasvolunteer/about/AboutController.java
@@ -2,6 +2,7 @@ package com.springrestfulhateoasvolunteer.about;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,7 @@ public class AboutController {
     private MessageSource messageSource;
 
     @GetMapping
-    public String getAbout(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
-        return messageSource.getMessage("about.message", null, locale);
+    public String getAbout() {
+        return messageSource.getMessage("about.message", null, LocaleContextHolder.getLocale());
     }
 }
--- src/main/java/com/springrestfulhateoasvolunteer/about/LocaleConfiguration.java
+++ src/main/java/com/springrestfulhateoasvolunteer/about/LocaleConfiguration.java
@@ -2,9 +2,8 @@ package com.springrestfulhateoasvolunteer.about;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 import java.util.Locale;
 
@@ -13,15 +12,8 @@ public class LocaleConfiguration {
 
     @Bean
     public LocaleResolver localeResolver() {
-        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
         localeResolver.setDefaultLocale(Locale.US);
         return localeResolver;
-    }
-
-    @Bean
-    public ResourceBundleMessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("about");
-        return messageSource;
     }
 }
